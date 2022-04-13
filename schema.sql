fcf5c720abff04cae3b8d98fa4e18b16cbf2690a
J-Seals <99369769+J-Seals@users.noreply.github.com>
2022-04-13 14:23:10 -0500

    Add files via upload

--- schema.sql
+++ schema.sql
@@ -47,9 +47,64 @@ CREATE TABLE titles (emp_no INT NOT NULL,
 	to_date DATE NOT NULL,
 	PRIMARY KEY (emp_no)
 );
+
+DROP TABLE dept_emp CASCADE;
+
+CREATE TABLE dept_emp (emp_no INT NOT NULL,
+	dept_no VARCHAR NOT NULL,
+	from_date DATE NOT NULL,
+	to_date DATE NOT NULL,
+	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+	FOREIGN KEY (dept_no) REFERENCES departments (dept_no)
+);
+
+DROP TABLE titles CASCADE;
+
+CREATE TABLE titles (emp_no INT NOT NULL,
+	title VARCHAR NOT NULL,
+	from_date DATE NOT NULL,
+	to_date DATE NOT NULL
+);
+
+SELECT * FROM titles
 												
-												
-SELECT * FROM departments;												
+-- Retirement Eligibility
+SELECT first_name, last_name
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+-- Born in 1952
+SELECT first_name, last_name
+FROM employees
+WHERE birth_date BETWEEN '1952-01-01' AND '1952-12-31';
+-- Born in 1953
+SELECT first_name, last_name
+FROM employees
+WHERE birth_date BETWEEN '1953-01-01' AND '1953-12-31';
+-- Born in 1954
+SELECT first_name, last_name
+FROM employees
+WHERE birth_date BETWEEN '1954-01-01' AND '1954-12-31';
+-- Born in 1955
+SELECT first_name, last_name
+FROM employees
+WHERE birth_date BETWEEN '1955-01-01' AND '1955-12-31';
+
+-- Get total number of employees retiring
+SELECT COUNT (first_name)
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+-- Create new table with retirement info
+SELECT first_name, last_name
+INTO retirement_info
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+
 												
 												
 									
